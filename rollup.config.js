c7b9deefcd13bb2a5f4b864f0f000eb79bc584ff
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-08-10 14:58:51 +0300

    removed the redundant ts prefix

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
-
 export default {
   input: './src/index.ts',
   output: [
