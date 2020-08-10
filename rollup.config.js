2ced0d7ee2b93b6c5144d520c6731dc7ff3acbab
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-08-10 14:58:56 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
+
 export default {
   input: './src/index.ts',
   output: [
