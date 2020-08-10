213a76ea634c6d53a559c1740043f2127c68de62
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-08-10 15:34:03 +0300

    add script for linting

--- src/hooks/useClickOutside/index.ts
+++ src/hooks/useClickOutside/index.ts
@@ -1,21 +1,20 @@
-
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
 function useClickOutside(ref: any, callback: Function) {
   function handleClickOutside(event: MouseEvent) {
-    if (ref.current && !ref.current.contains(event.target)) {
-      callback();
+    if (!ref?.current?.contains(event.target)) {
+      callback()
     }
   }
 
   useEffect(() => {
     // Bind the event listener
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside)
     return () => {
       // Unbind the event listener on clean up
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  });
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
 }
 
-export default useClickOutside;
+export default useClickOutside
